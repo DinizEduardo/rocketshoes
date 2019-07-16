2d013d27dc3114aa8d51934a6b6805a24a86fcd2
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-16 19:52:26 -0300

    Organização de codigo usando o saga

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,13 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 // para adicionar outros reducers cria a pasta e importa dentro do rootReducer
-
-const SagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const SagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
